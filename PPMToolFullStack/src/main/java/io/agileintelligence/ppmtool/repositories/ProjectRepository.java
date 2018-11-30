99996922764830a8d8c369526bb29e9f97a3cb75
beingthenewyou <beingthenewyou@gmail.com>
2018-11-30 12:25:00 +0800

    First create API

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
@@ -4,6 +4,5 @@ import io.agileintelligence.ppmtool.domain.Project;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProjectRepository extends CrudRepository<Project,Long> {
-    @Override
-    Iterable<Project> findAllById(Iterable<Long> iterable);
+
 }
